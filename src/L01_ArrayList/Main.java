3f0b18e9e4652396ddfadc15c658a3715d84101b
thenghiabk <thenghiabk@gmail.com>
2026-01-21 10:22:21 +0700

    added ArrayList

--- src/L01_ArrayList/Main.java
+++ src/L01_ArrayList/Main.java
@@ -1,18 +1,35 @@
 package L01_ArrayList;
 
-class ArrayList<E> {
+import java.util.Arrays;
+
+class ArrayListADT<E> {
     // attributes
     private E[] elements;
     private int nextIndex;
 
     // constructor
-    public ArrayList(){
+    public ArrayListADT(){
         elements = (E[]) new Object[5];
         nextIndex = 0;
     }
 
     // helpers
     public boolean add(E element){
+        if (nextIndex == elements.length){
+
+            // Option 1
+//            E[] newElements = (E[]) new Object[elements.length * 2];
+//
+//            for (int i = 0; i < elements.length; i++){
+//                newElements[i] = elements[i];
+//            }
+//
+//            this.elements = newElements;
+
+            // Option 2
+            this.elements = Arrays.copyOf(this.elements, this.elements.length * 2);
+        }
+
         this.elements[nextIndex] = element;
         nextIndex++;
         return true;
@@ -21,6 +38,17 @@ class ArrayList<E> {
 
 public class Main {
     public static void main(String[] args) {
-
+        try{
+            ArrayListADT<Integer> myArrayList = new ArrayListADT<>();
+            myArrayList.add(10);
+            myArrayList.add(20);
+            myArrayList.add(30);
+            myArrayList.add(40);
+            myArrayList.add(50);
+            myArrayList.add(60);
+            System.out.println();
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
